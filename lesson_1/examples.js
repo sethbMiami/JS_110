736e8b507ca43c734c597b459b8d5619159c125c
Seth Bruner <bruner.seth@outlook.com>
2023-12-27 17:00:47 -0500

    nested loop for palindrome substrings

--- lesson_1/examples.js
+++ lesson_1/examples.js
@@ -6,11 +6,11 @@ let randomObject = {
   broccoli: 'vegetable'
 };
 
-console.log(randomObject);
+// console.log(randomObject);
 
-randomObject.apple = 'yum';
+// randomObject.apple = 'yum';
 
-console.log(randomObject);
+// console.log(randomObject);
 
 let randomString = "Hello, how, are, you, doing?";
 console.log(randomString.split(", "));
@@ -18,3 +18,42 @@ let bUnicode = String.fromCharCode(98);
 let letterB = bUnicode.charCodeAt();
 console.log(bUnicode);
 console.log(letterB);
+
+for (let index in randomObject) {
+  console.log(`${index} is a ${randomObject[index]}`);
+}
+
+
+const substringArray = (string) => {
+  let subArray = [];
+
+  for (let index = 0; index < string.length - 1; index++) {
+    for (let index2 = index; index2 < string.length - 1; index2++) {
+      let substr = string.slice(index, index2 + 2);
+      subArray.push(substr);
+    }
+  }
+  return subArray;
+};
+
+const palindromeCheck = (string) => {
+  return string === string.split("").reverse().join("");
+};
+
+const palindromeSubstrings = (string) => {
+  let paliArray = [];
+
+  substringArray(string).forEach((substr) => {
+    if (palindromeCheck(substr)) {
+      paliArray.push(substr);
+    }
+  });
+
+  return paliArray;
+};
+
+
+console.log(palindromeSubstrings("supercalifragilisticexpialidocious")); // ["ili"]
+console.log(palindromeSubstrings("abcddcbA"));   // ["bcddcb", "cddc", "dd"]
+console.log(palindromeSubstrings("palindrome")); // []
+console.log(palindromeSubstrings("")); // []
