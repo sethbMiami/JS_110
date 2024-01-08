284d42e0d1cd80308252a965c25cea94bbe7cc43
Seth Bruner <bruner.seth@outlook.com>
2024-01-08 11:44:57 -0500

    lesson 3 programs

---
+++ lesson_3/scrap.js
@@ -0,0 +1,1 @@
+// program walk-thru
--- practiceProblems.js
+++ practiceProblems.js
@@ -282,3 +282,42 @@ console.log(createUUID());
 console.log(createUUID());
 
 
+const ARR = [
+  { one: '1', two: 2 },
+  [ { four: 5, three: 6 }, 'three' ],
+  'three',
+  { '2': 'two', '3': 'three' }
+];
+
+console.log(ARR[1][0].three);
+
+let todoLists = [
+  {
+    id: 1,
+    listName: 'Groceries',
+    todos: [
+      { id: 1, name: 'Bread', completed: false },
+      { id: 2, name: 'Milk', completed: false },
+      { id: 3, name: 'Apple Juice', completed: false }
+    ]
+  }
+];
+
+todoLists[0]['todos'][2].name = 'Orange Juice';
+console.log(todoLists[0].todos);
+
+function evenValues(array) {
+  let evens = [];
+
+  array.forEach(value => {
+    if (value % 2 === 0) {
+      evens.push(value);
+    }
+    array.shift();
+  });
+
+  return evens;
+}
+
+console.log(evenValues([1, 3, 4, 2, 4, 6, 5, 7, 9, 10, 12]));
+
